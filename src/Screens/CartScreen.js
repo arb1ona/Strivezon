f5020934e21ba9822e09262df0e063788e8ff4ab
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-08-12 23:23:38 +0200

    update comment

--- src/Screens/CartScreen.js
+++ src/Screens/CartScreen.js
@@ -2,18 +2,17 @@ import React, { useEffect } from 'react';
 import { addToCart, removeFromCart } from '../actions/cartActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+// function component + redux property 
 function CartScreen(props) {
-
     const cart = useSelector(state => state.cart);
-
     const { cartItems } = cart;
-
     const productId = props.match.params.id;
     const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
     const dispatch = useDispatch();
     const removeFromCartHandler = (productId) => {
         dispatch(removeFromCart(productId));
     }
+    
     useEffect(() => {
         if (productId) {
             dispatch(addToCart(productId, qty));
