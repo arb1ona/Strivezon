4c6436edc025b8b175ca7db6a0569fd4fa74784b
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-08-30 23:33:54 +0200

    Update ProductsScreen.js

--- src/Screens/ProductsScreen.js
+++ src/Screens/ProductsScreen.js
@@ -5,53 +5,43 @@ import { signin } from '../actions/userActions';
 
 
 function ProductsScreen(props) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const submitHandler = (e) => {
-        e.preventDefault();
-        dispatch(signin(email, password))
-    }
-    const userSignin = useSelector(state => state.userSignin)
-    const { loading, userInfo, error } = userSignin
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const productSave = useSelector(state => state.productSave)
+    const { loading: loadingSave, success: successSave, error: errorSave } = productSave;
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if (userInfo) {
-            props.history.push("/")
-        }
+
         return () => {
             //
         }
     }, [userInfo])
 
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(saveProduct({ name, price }))
+    }
+
     return <div className="form">
         <form onSubmit={submitHandler}>
             <ul className="form-container">
                 <li>
-                    <h2>Sign-In</h2>
+                    <h2>Create Product</h2>
                 </li>
                 <li>
                     {loading && <div>Loading...</div>}
                     {error && <div>error...</div>}
                 </li>
                 <li>
-                    <label htmlFor="email">
-                        Email
+                    <label htmlFor="name">
+                        Name
                     </label>
-                    <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}></input>
+                    <input type="email" name="name" id="name" onChange={(e) => setName(e.target.value)}></input>
                 </li>
+
                 <li>
-                    <label htmlFor="password">
-                        Password
-                    </label>
-                    <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)}></input>
-                </li>
-                <li>
-                    <button type="submit" className="button-primary">Signin</button>
-                </li>
-                <li>New to Amazona?</li>
-                <li>
-                    <Link to="/register" className="button-secondary">Create your amazona account</Link>
+                    <button type="submit" className="button-primary">Create</button>
                 </li>
             </ul>
 
